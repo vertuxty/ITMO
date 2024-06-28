facf1c76b07869d55c130385df8868c8d78f19b5
Leshchev Vladimir <l-or1alex@mail.ru>
2024-06-28 11:50:44 +0500

    del old web-course version

--- Web-course/hw10/src/fun.js
+++
@@ -1,7 +0,0 @@
-export const commentsCount = function (post, comments) {
-    return postComments(post, comments).length;
-}
-
-export const postComments = function (post, comments) {
-    return Object.values(comments).filter(c => c.postId === post.id);
-}
--- Web-course/hw6/src/main/java/ru/itmo/wp/model/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.itmo.wp.model.repository;
-
-import ru.itmo.wp.model.domain.Event;
-import ru.itmo.wp.model.domain.User;
-
-public interface EventRepository {
-    void save(Event event);
-}
